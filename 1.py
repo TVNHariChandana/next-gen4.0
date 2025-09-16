69be632d6b7f2eefedc6ff60505af4aabc3ffeb5
TVNHariChandana <tummalacheruvuharichandana@gmail.com>
2025-09-16 11:03:47 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+'''import statistics as stats
+ 
+data=[10,20,20,40,50]
+print("Mean",stats.mean(data))
+print("Median",stats.median(data))
+print("Mode",stats.mode(data))
+print("standard deviation",stats.stdev(data))'''
+'''import math
+x=16
+print("square root:",math.sqrt(x))
+print("factorial of 5:",math.factorial(5))
+print("log base of 2 of 16:",math.log2(x))
+print("cosine of 45:",math.cos(math.radians(45)))'''
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print("array:",arr)
+print("Mean:",np.mean(arr))
+print("standard deviation
+ 
